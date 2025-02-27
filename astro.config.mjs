8c4088589bcc71211fa73ef597a0355b4c7a8671
craigmaclean <craigmaclean1@gmail.com>
2025-02-27 13:54:18 -0800

    moved blog to content collection format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://astro-tutorial-build.netlify.app/"
+  site: "https://astro-tutorial-build.netlify.app/",
+  integrations: [preact()]
 });
